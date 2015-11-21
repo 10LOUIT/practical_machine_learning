22a0aeacad8660aa7450c045663dd49a64cac9c1
Thor <thorsten.louis@gmx.de>
2015-11-21 20:10:57 +0100

    week 3

---
+++ bagging.R
@@ -0,0 +1,34 @@
+library(sandwich)
+library(grid)
+library(mvtnorm)
+library(modeltools)
+library(stats4)
+library(strucchange)
+library(zoo)
+library(party)
+library(ElemStatLearn)
+library(lattice)
+library(ggplot2)
+library(caret)
+data(ozone,package = "ElemStatLearn")
+ozone <- ozone[order(ozone$ozone),]
+head(ozone)
+ll <- matrix(NA, nrow = 10, ncol = 155)
+for(i in 1:10){
+  ss <- sample(1:dim(ozone)[1], replace=T)
+  ozone0 <- ozone[ss,]
+  ozone0 <- ozone0[order(ozone0$ozone),]
+  loess0 <- loess(temperature ~ ozone,data=ozone0, span = 0.2)
+  ll[i,] <- predict(loess0, newdata=data.frame(ozone=1:155))
+}
+plot(ozone$ozone, ozone$temperature, pch=19, cex=0.5)
+for(i in 1:10){ lines(1:155, ll[i,], col="grey", lwd=2)}
+lines(1:155, apply(ll,2,mean),col="red", lwd=2)
+
+predictors = data.frame(ozone=ozone$ozone)
+temperature = ozone$temperature
+treebag <- bag(predictors, temperature, B = 10, bagControl = bagControl(fit = ctreeBag$fit, predict = ctreeBag$pred, aggregate = ctreeBag$aggregate))
+
+plot(ozone$ozone,temperature, col="lightgrey", pch=19)
+points(ozone$ozone,predict(treebag$fits[[1]]$fit, predictors),pch=19, col="red")
+points(ozone$ozone,predict(treebag, predictors),pch=19, col="blue")
---
+++ predicting_with_dicision_trees.R
@@ -0,0 +1,23 @@
+data(iris)
+library(lattice)
+library(ggplot2)
+library(caret)
+names(iris)
+table(iris$Species)
+inTrain <- createDataPartition(y=iris$Species, p=0.7, list=FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+dim(training)
+dim(testing)
+qplot(Petal.Width, Sepal.Width, colour=Species, data=training)
+
+library(e1071)
+library(rpart)
+modFit <- train(Species ~ ., method="rpart", data=training)
+print(modFit$finalModel)
+plot(modFit$finalModel, uniform = TRUE, main="Classification Tree")
+text(modFit$finalModel, use.n = TRUE, all=TRUE, cex=.8)
+library(rattle)
+library(rpart.plot)
+fancyRpartPlot(modFit$finalModel)
+predict(modFit, newdata=testing)
